43cb82357ed2caab09c86bb119aa4d1fa6c7fb44
ajung26 <ajung26@csus.org>
2024-01-20 18:51:54 -0800

    ArmAmpCommand and ArmSpeakerCommand

--- src/main/java/frc/robot/commands/ArmSpeakerCommand.java
+++ src/main/java/frc/robot/commands/ArmSpeakerCommand.java
@@ -1,9 +1,10 @@
+//Allison B)
+
 package frc.robot.commands;
 import frc.robot.subsystems.ArmSubsystem;
 
 public class ArmSpeakerCommand {
     private ArmSubsystem arm_subsystem;
-    private double speed = 0/*SPEED */;
 
     public ArmSpeakerCommand(ArmSubsystem arm_subsystem){
         this.arm_subsystem = arm_subsystem;
