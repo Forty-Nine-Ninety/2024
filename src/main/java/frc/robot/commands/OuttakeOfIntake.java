a40932b05cdf07fd97482e5061f0357780b80526
Shreyans J <145509892+shreyanscrystal@users.noreply.github.com>
2024-01-22 16:55:16 -0800

    Created Outtake Of Intake Command
    EVEN MORE EZ 7th thing I did EZZZZZ LETS GOOGGOOGGOO

---
+++ src/main/java/frc/robot/commands/OuttakeOfIntake.java
@@ -0,0 +1,22 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class OuttakeOfIntake extends Command {
+    private final IntakeSubsystem m_intakeSubsystem;
+
+    public OuttakeOfIntake(IntakeSubsystem intakeSubsystem){
+        m_intakeSubsystem = intakeSubsystem;
+    }
+
+    @Override
+    public void initialize(){
+       m_intakeSubsystem.intakePercentOutput(-1);
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        m_intakeSubsystem.intakePercentOutput(0);
+    }
+}
