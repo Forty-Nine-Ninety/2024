3b509ccc516001219064c5653407af876cce1532
ajung26 <ajung26@csus.org>
2024-01-27 14:01:34 -0800

    AutoCommand

--- src/main/java/frc/robot/commands/Auto/AutoCommand.java
+++ src/main/java/frc/robot/commands/Auto/AutoCommand.java
@@ -1,12 +1,4 @@
-package frc.robot.commands;
-
-public class AutoCommand1 {
-
-    // Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-package frc.robot.commands.swervedrive.auto;
+package frc.robot.commands.Auto;
 
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
@@ -19,25 +11,22 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import frc.robot.Constants.Auton;
-import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import java.util.HashMap;
 import java.util.List;
 
-public final class Autos
-{
-
+public class AutoCommand{
   /**
    * April Tag field layout.
    */
   private static AprilTagFieldLayout aprilTagField = null;
 
-  private Autos()
+  private AutoCommand()
   {
     throw new UnsupportedOperationException("This is a utility class!");
   }
@@ -188,11 +177,5 @@ public final class Autos
 //    swerve.postTrajectory(example);
     return Commands.sequence(new FollowTrajectory(swerve, example, true));
   }
-
-
-
-
-
-
 
 }
