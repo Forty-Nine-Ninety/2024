0d0e98ee1158221f943514dcc0f42489a68b3fbd
chloe295 <117236061+chloe295@users.noreply.github.com>
2024-01-27 15:08:11 -0800

    BreakBeam added to shooter subsystem
    Added a breakbeam function to shootersubsystem to detect if a breakbeam is being obstructed.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,21 +5,23 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.MotionControl;
 import frc.robot.Constants.*;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class ShooterSubsystem extends SubsystemBase {
-    private CANSparkMax shooter1, shooter2, shooter3;
+    private CANSparkMax shooter1, shooter2;
     private SparkPIDController m_pidController;
     private RelativeEncoder m_encoder;
+    private final DigitalInput m_breakbeam;
 
     
     public ShooterSubsystem() {
-        shooter1 = new CANSparkMax(Ports.CAN_SHOOTER_ONE_SPARKMAX, MotorType.kBrushless); //Note, I don't know the CAN IDs, will put these in later -Chloe
+        shooter1 = new CANSparkMax(Ports.CAN_SHOOTER_ONE_SPARKMAX, MotorType.kBrushless); 
         shooter2 = new CANSparkMax(Ports.CAN_SHOOTER_TWO_SPARKMAX, MotorType.kBrushless);
-
+        m_breakbeam = new DigitalInput(Ports.PORT_DIO_BREAK_BEAM); 
         m_pidController = shooter1.getPIDController();
         m_encoder = shooter1.getEncoder();
       
@@ -41,7 +43,7 @@ public class ShooterSubsystem extends SubsystemBase {
         shooter1.setOpenLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
         shooter2.setOpenLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
 
-        //Need motor inversion?? 
+        //Is motor inversion necessary?
 
         shooter1.setSmartCurrentLimit(22, 15);
         shooter2.setSmartCurrentLimit(22, 15);
@@ -49,5 +51,10 @@ public class ShooterSubsystem extends SubsystemBase {
 
     public void percentOutput(double percent_output) {
         shooter1.set(percent_output);
+    }
+
+    public boolean breakBeam() {
+        //Detects if anything is obstructing the break beam 
+        return m_breakbeam.get();
     }
 }
