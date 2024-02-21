208a5f0078908b6866e688c810c18bdb6e90dddb
Megatron1234567890 <ilee24@crystal.org>
2024-02-20 16:46:07 -0800

    Review Shooter Code

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -31,11 +31,10 @@ public class ShooterSubsystem extends SubsystemBase {
     private void configureMotors() {
         shooter1.restoreFactoryDefaults(); 
         shooter2.restoreFactoryDefaults(); 
-
-        m_pidController = shooter1.getPIDController();
  
-        m_pidController.setP(MotionControl.SHOOTER_PID[1]);
-        m_pidController.setP(MotionControl.SHOOTER_PID[2]);
+        //m_pidController.setP(MotionControl.SHOOTER_PID[0]);
+        //m_pidController.setI(MotionControl.SHOOTER_PID[1]);
+        //m_pidController.setD(MotionControl.SHOOTER_PID[2]);
 
         shooter1.setClosedLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
         shooter2.setClosedLoopRampRate(MotionControl.CLOSED_LOOP_RAMP_RATE);
