24398c8967160b3190f7548c1aabff962766a484
Megatron1234567890 <ilee24@crystal.org>
2024-02-24 14:18:53 -0800

    Adjust indentation for readability

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,99 +21,99 @@ import swervelib.math.Matter;
  */
 public final class Constants
 {
-  //public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+    //public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
 
-  public static class Ports
-  {
-    public static int PORT_JOYSTICK_DRIVE = 0;
-  }
-  
-  public static class MotorConfig
-  {
-    public static double CANANDCODER_RESOLUTION = 16384.0;
-  }
+    public static class Ports
+    {
+        public static int PORT_JOYSTICK_DRIVE = 0;
+    }
+    
+    public static class MotorConfig
+    {
+        public static double CANANDCODER_RESOLUTION = 16384.0;
+    }
 
-  public static class RobotMeasurements
-  {
-    public static double DRIVETRAIN_WHEEL_DIAMETER_IN = 4.0;
-    public static final double DRIVE_GEAR_RATIO = 6.75;
-    public static final double STEERING_GEAR_RATIO = 150.0/7.0;
+    public static class RobotMeasurements
+    {
+        public static double DRIVETRAIN_WHEEL_DIAMETER_IN = 4.0;
+        public static final double DRIVE_GEAR_RATIO = 6.75;
+        public static final double STEERING_GEAR_RATIO = 150.0/7.0;
 
-    //public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
-    //public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
-  }
+        //public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+        //public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    }
 
-  public static class OurUnits
-  {
-    //Base units
-    public static Unit METER = new BaseUnit(Dimension.Length, 1d);
-    public static Unit KILOMETER = new BaseUnit(Dimension.Length, METER.getScalar() * 1000d);
-    public static Unit FEET = new BaseUnit(Dimension.Length, METER.getScalar() * 3.280839895d);
-    public static Unit INCH = new BaseUnit(Dimension.Length, FEET.getScalar() / 12d);
+    public static class OurUnits
+    {
+        //Base units
+        public static Unit METER = new BaseUnit(Dimension.Length, 1d);
+        public static Unit KILOMETER = new BaseUnit(Dimension.Length, METER.getScalar() * 1000d);
+        public static Unit FEET = new BaseUnit(Dimension.Length, METER.getScalar() * 3.280839895d);
+        public static Unit INCH = new BaseUnit(Dimension.Length, FEET.getScalar() / 12d);
 
-    public static Unit SECOND = new BaseUnit(Dimension.Time, 1d);
-    public static Unit MINUTE = new BaseUnit(Dimension.Time, SECOND.getScalar() * 60d);
-    public static Unit HOUR = new BaseUnit(Dimension.Time, MINUTE.getScalar() * 60d);
-    public static Unit MILLISECOND = new BaseUnit(Dimension.Time, SECOND.getScalar() / 1000d);
-    //public static Unit ENCODER_TIME = new BaseUnit(Dimension.Time, MILLISECOND.getScalar() * 100d);
-        
-    public static Unit KILOGRAM = new BaseUnit(Dimension.Mass, 1d);
+        public static Unit SECOND = new BaseUnit(Dimension.Time, 1d);
+        public static Unit MINUTE = new BaseUnit(Dimension.Time, SECOND.getScalar() * 60d);
+        public static Unit HOUR = new BaseUnit(Dimension.Time, MINUTE.getScalar() * 60d);
+        public static Unit MILLISECOND = new BaseUnit(Dimension.Time, SECOND.getScalar() / 1000d);
+        //public static Unit ENCODER_TIME = new BaseUnit(Dimension.Time, MILLISECOND.getScalar() * 100d);
+                
+        public static Unit KILOGRAM = new BaseUnit(Dimension.Mass, 1d);
 
-    public static Unit RADIAN = new BaseUnit(Dimension.Angle, 1d);
-    public static Unit REVOLUTION = new BaseUnit(Dimension.Angle, RADIAN.getScalar() * 2d * Math.PI);
-    public static Unit DEGREE = new BaseUnit(Dimension.Angle, REVOLUTION.getScalar() / 360d);
-    // Angle represented by encoder ticks, i.e 16384 ticks is a full revolution
-    public static Unit ENCODER_ANGLE = new BaseUnit(Dimension.Angle, REVOLUTION.getScalar() / MotorConfig.CANANDCODER_RESOLUTION);
+        public static Unit RADIAN = new BaseUnit(Dimension.Angle, 1d);
+        public static Unit REVOLUTION = new BaseUnit(Dimension.Angle, RADIAN.getScalar() * 2d * Math.PI);
+        public static Unit DEGREE = new BaseUnit(Dimension.Angle, REVOLUTION.getScalar() / 360d);
+        // Angle represented by encoder ticks, i.e 16384 ticks is a full revolution
+        public static Unit ENCODER_ANGLE = new BaseUnit(Dimension.Angle, REVOLUTION.getScalar() / MotorConfig.CANANDCODER_RESOLUTION);
 
-    public static Unit AMPERE = new BaseUnit(Dimension.Current, 1d);
+        public static Unit AMPERE = new BaseUnit(Dimension.Current, 1d);
 
-    //Compound units
-    //public static Unit ENCODER_VELOCITY_UNIT = new CompoundUnit(ENCODER_ANGLE, ENCODER_TIME);
-    public static Unit ENCODER_ANGULAR_VELOCITY = new CompoundUnit(ENCODER_ANGLE, SECOND);
+        //Compound units
+        //public static Unit ENCODER_VELOCITY_UNIT = new CompoundUnit(ENCODER_ANGLE, ENCODER_TIME);
+        public static Unit ENCODER_ANGULAR_VELOCITY = new CompoundUnit(ENCODER_ANGLE, SECOND);
 
-    public static Unit ANGULAR_VELOCITY = new CompoundUnit(RADIAN, SECOND);
-    public static Unit METERS_PER_SECOND = new CompoundUnit(METER, SECOND);
+        public static Unit ANGULAR_VELOCITY = new CompoundUnit(RADIAN, SECOND);
+        public static Unit METERS_PER_SECOND = new CompoundUnit(METER, SECOND);
 
-    public static Unit METERS_PER_SECOND_2 = new CompoundUnit(METERS_PER_SECOND, SECOND);
-    public static Unit NEWTON = new CompoundUnit(new Unit[] {KILOGRAM, METERS_PER_SECOND_2}, new Unit[] {});
-    public static Unit JOULE = new CompoundUnit(new Unit[] {NEWTON, METER}, new Unit[] {});
-    public static Unit COULOMB = new CompoundUnit(new Unit[] {AMPERE, SECOND}, new Unit[] {});
-    public static Unit VOLTAGE = new CompoundUnit(JOULE, COULOMB);
-  }
+        public static Unit METERS_PER_SECOND_2 = new CompoundUnit(METERS_PER_SECOND, SECOND);
+        public static Unit NEWTON = new CompoundUnit(new Unit[] {KILOGRAM, METERS_PER_SECOND_2}, new Unit[] {});
+        public static Unit JOULE = new CompoundUnit(new Unit[] {NEWTON, METER}, new Unit[] {});
+        public static Unit COULOMB = new CompoundUnit(new Unit[] {AMPERE, SECOND}, new Unit[] {});
+        public static Unit VOLTAGE = new CompoundUnit(JOULE, COULOMB);
+    }
 
-  public static class SubsystemConfig
-  {
-    //Movement information
+    public static class SubsystemConfig
+    {
+        //Movement information
 
-    // Hold time on motor brakes when disabled
-    public static final double WHEEL_LOCK_TIME = 10; // seconds
-  }
+        // Hold time on motor brakes when disabled
+        public static final double WHEEL_LOCK_TIME = 10; // seconds
+    }
 
-  public static class MotionControl
-  {
-  }
+    public static class MotionControl
+    {
+    }
 
-  public static final class AutonConstants
-  {
-    public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
-    public static final PIDConstants ANGLE_PID   = new PIDConstants(0.4, 0, 0.01);
-  }
+    public static final class AutonConstants
+    {
+        public static final PIDConstants TRANSLATION_PID = new PIDConstants(0.7, 0, 0);
+        public static final PIDConstants ANGLE_PID   = new PIDConstants(0.4, 0, 0.01);
+    }
 
-  public static class DriveSettings
-  {
-    // Joystick Deadband
-    public static final double LEFT_X_DEADBAND  = 0.1;
-    public static final double LEFT_Y_DEADBAND  = 0.1;
-    public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double TURN_CONSTANT    = 6;
+    public static class DriveSettings
+    {
+        // Joystick Deadband
+        public static final double LEFT_X_DEADBAND  = 0.1;
+        public static final double LEFT_Y_DEADBAND  = 0.1;
+        public static final double RIGHT_X_DEADBAND = 0.1;
+        public static final double TURN_CONSTANT    = 6;
 
-    //Driver settings
-    public static double JOYSTICKF310_AXIS_DEADBAND = 0.01;
-    public static double JOYSTICK_THROTTLE_X_EXPONENT = 3;
-    public static double JOYSTICK_THROTTLE_Y_EXPONENT = 3;
-    public static double JOYSTICK_TURNING_EXPONENT = 3;
-    public static double ARCADE_SPEED_X_MULTIPLIER = -1.0;
-    public static double ARCADE_SPEED_Y_MULTIPLIER = -1.0;
-    public static double ARCADE_ROTATION_MULTIPLIER = 1.0;
-  }
+        //Driver settings
+        public static double JOYSTICKF310_AXIS_DEADBAND = 0.01;
+        public static double JOYSTICK_THROTTLE_X_EXPONENT = 3;
+        public static double JOYSTICK_THROTTLE_Y_EXPONENT = 3;
+        public static double JOYSTICK_TURNING_EXPONENT = 3;
+        public static double ARCADE_SPEED_X_MULTIPLIER = -1.0;
+        public static double ARCADE_SPEED_Y_MULTIPLIER = -1.0;
+        public static double ARCADE_ROTATION_MULTIPLIER = 1.0;
+    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,98 +33,98 @@ import com.pathplanner.lib.auto.AutoBuilder;
  */
 public class RobotContainer
 {
-  JoystickF310 joystickDrive = new JoystickF310(Ports.PORT_JOYSTICK_DRIVE);
+    JoystickF310 joystickDrive = new JoystickF310(Ports.PORT_JOYSTICK_DRIVE);
 
-  // The robot's subsystems and commands are defined here...
-  private final SwerveSubsystem m_drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+    // The robot's subsystems and commands are defined here...
+    private final SwerveSubsystem m_drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
 
-  private final DriveCommand m_driveCommand = new DriveCommand(m_drivebase);
-  private final DriveSimulationCommand m_driveSimulationCommand = new DriveSimulationCommand(m_drivebase);
+    private final DriveCommand m_driveCommand = new DriveCommand(m_drivebase);
+    private final DriveSimulationCommand m_driveSimulationCommand = new DriveSimulationCommand(m_drivebase);
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer()
-  {
-    // Configure the trigger bindings
-    configureBindings();
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer()
+    {
+        // Configure the trigger bindings
+        configureBindings();
 
-    // Applies deadbands and inverts controls because joysticks
-    // are back-right positive while robot
-    // controls are front-left positive
-    // left stick controls translation
-    // right stick controls the desired angle NOT angular rotation
-    Command driveFieldOrientedDirectAngle = m_drivebase.driveCommand(
-        () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
-        () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
-        () -> -joystickDrive.getRawAxis(AxisF310.JoystickRightX),
-        () -> -joystickDrive.getRawAxis(AxisF310.JoystickRightY));
+        // Applies deadbands and inverts controls because joysticks
+        // are back-right positive while robot
+        // controls are front-left positive
+        // left stick controls translation
+        // right stick controls the desired angle NOT angular rotation
+        Command driveFieldOrientedDirectAngle = m_drivebase.driveCommand(
+                () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
+                () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
+                () -> -joystickDrive.getRawAxis(AxisF310.JoystickRightX),
+                () -> -joystickDrive.getRawAxis(AxisF310.JoystickRightY));
 
-    Command driveFieldOrientedDirectAngleSim = m_drivebase.simDriveCommand(
-        () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
-        () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
-        () -> -joystickDrive.getRawAxis(2));
+        Command driveFieldOrientedDirectAngleSim = m_drivebase.simDriveCommand(
+                () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
+                () -> -MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
+                () -> -joystickDrive.getRawAxis(2));
 
-    //m_drivebase.setDefaultCommand(
-        //!RobotBase.isSimulation() ? driveFieldOrientedDirectAngle : driveFieldOrientedDirectAngleSim);
-  }
+        //m_drivebase.setDefaultCommand(
+                //!RobotBase.isSimulation() ? driveFieldOrientedDirectAngle : driveFieldOrientedDirectAngleSim);
+    }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
-   * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-   * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
-   * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
-   */
-  private void configureBindings()
-  {
-    m_driveCommand.setSuppliers(
-      () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
-      () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
-      () -> joystickDrive.getRawAxis(AxisF310.JoystickRightX),
-      () -> joystickDrive.getRawAxis(AxisF310.JoystickRightY)
-    );
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
+     * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+     * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
+     * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
+     */
+    private void configureBindings()
+    {
+        m_driveCommand.setSuppliers(
+            () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
+            () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
+            () -> joystickDrive.getRawAxis(AxisF310.JoystickRightX),
+            () -> joystickDrive.getRawAxis(AxisF310.JoystickRightY)
+        );
 
-    m_driveSimulationCommand.setSuppliers(
-      () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
-      () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
-      () -> joystickDrive.getRawAxis(2)
-    );
+        m_driveSimulationCommand.setSuppliers(
+            () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftY), DriveSettings.LEFT_Y_DEADBAND),
+            () -> MathUtil.applyDeadband(joystickDrive.getRawAxis(AxisF310.JoystickLeftX), DriveSettings.LEFT_X_DEADBAND),
+            () -> joystickDrive.getRawAxis(2)
+        );
 
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
-    joystickDrive.getButton(ButtonF310.A).onTrue((Commands.runOnce(m_drivebase::zeroGyro)));
-    //joystickDrive.getButton(ButtonF310.B).onTrue(Commands.runOnce(m_drivebase::addFakeVisionReading));
-    //joystickDrive.getButton(ButtonF310.X).whileTrue(
-        //Commands.deferredProxy(() -> m_drivebase.driveToPose(
-                                   //new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
-                              //));
-    //  joystickDrive.getButton(ButtonF310.Y).whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
-  }
-  
-  public void setTeleopDefaultCommands()
-  {
-    CommandScheduler.getInstance().setDefaultCommand(m_drivebase, !RobotBase.isSimulation() ? m_driveCommand : m_driveSimulationCommand);
-  }
+        joystickDrive.getButton(ButtonF310.A).onTrue((Commands.runOnce(m_drivebase::zeroGyro)));
+        //joystickDrive.getButton(ButtonF310.B).onTrue(Commands.runOnce(m_drivebase::addFakeVisionReading));
+        //joystickDrive.getButton(ButtonF310.X).whileTrue(
+                //Commands.deferredProxy(() -> m_drivebase.driveToPose(
+                                                                     //new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
+                                                            //));
+        //  joystickDrive.getButton(ButtonF310.Y).whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+    }
+    
+    public void setTeleopDefaultCommands()
+    {
+        CommandScheduler.getInstance().setDefaultCommand(m_drivebase, !RobotBase.isSimulation() ? m_driveCommand : m_driveSimulationCommand);
+    }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand()
-  {
-    // An example command will be run in autonomous
-    return m_drivebase.getAutonomousCommand("New Auto");
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand()
+    {
+        // An example command will be run in autonomous
+        return m_drivebase.getAutonomousCommand("New Auto");
+    }
 
-  public void setDriveMode()
-  {
-    //drivebase.setDefaultCommand();
-  }
+    public void setDriveMode()
+    {
+        //drivebase.setDefaultCommand();
+    }
 
-  public void setMotorBrake(boolean brake)
-  {
-    m_drivebase.setMotorBrake(brake);
-  }
+    public void setMotorBrake(boolean brake)
+    {
+        m_drivebase.setMotorBrake(brake);
+    }
 }
