477d783872b0789fe62e7cf2d8bb076791e7eb44
ajung26 <ajung26@csus.org>
2024-03-01 00:09:57 -0800

    Regurgitation

--- src/main/java/frc/robot/commands/RegurgitationCommand.java
+++ src/main/java/frc/robot/commands/RegurgitationCommand.java
@@ -17,6 +17,7 @@ public class RegurgitationCommand extends Command {
 
     @Override
     public void execute(){
+        m_intakeSubsystem.extendIntake();
         m_intakeSubsystem.intakePercentOutput(-1);
         m_shooterSubsystem.percentOutput(-1);
     }
@@ -24,5 +25,6 @@ public class RegurgitationCommand extends Command {
     public void end(boolean interrupted){
         m_intakeSubsystem.intakePercentOutput(0.0);
         m_shooterSubsystem.percentOutput(0.0);
+        m_intakeSubsystem.retractIntake();
     }
 }
