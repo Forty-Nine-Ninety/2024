81c52e6555d09fbcc0135a72d7e5b4da3b0ffdf1
ajung26 <ajung26@csus.org>
2024-04-01 14:35:31 -0700

    Update IntakeExtendCommand.java

--- src/main/java/frc/robot/commands/IntakeExtendCommand.java
+++ src/main/java/frc/robot/commands/IntakeExtendCommand.java
@@ -3,8 +3,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
