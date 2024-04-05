d0da304c69572cba1f3513b18d74ef7d5e72df22
ajung26 <ajung26@csus.org>
2024-04-04 18:59:53 -0700

    Auto

--- src/main/java/frc/robot/commands/Auto/Auto11NBlueCommand.java
+++ src/main/java/frc/robot/commands/Auto/Auto11NBlueCommand.java
@@ -15,35 +15,9 @@ public class Auto11NBlueCommand extends SequentialCommandGroup{
         m_drivebase = drivebase;
         m_arm = arm;
         m_shooter = shooter;
-        addCommands(new AutoSpeakerCommand(m_shooter, m_arm),
+        addCommands(new AutoSpeakerCommand(m_shooter, m_arm).withTimeout(4.05),
                     new WaitCommand(0),
                     m_drivebase.getAutonomousCommand("11NBlue")
         );
-
     }
-
-    /*public SequentialCommandGroup getAutonomousCommand() {
-        // Create voltage constraint to ensure we don't accelerate too fast
-        /*var autoVoltageConstraint = 
-            new DifferentialDriveVoltageConstraint(
-                new SimpleMotorFeedforward(
-                    MotionControl.ksVolts,
-                    MotionControl.kvVoltSecondsPerMeter),
-                MotionControl.kDriveKinematics,
-                SubsystemConfig.AUTO_MAX_VOLTAGE);*/
-        
-        // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-        /*PathPlannerAuto path = new PathPlannerAuto("Auto11NBlue");
-        path.put("Note 1",new AutoSpeakerCommand(arm_subsystem,shooter_subsystem))
-        List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile("Auto11NBlue");
-        //PathPlannerTrajectory path11NBlueAuto = new PathPlannerTrajectory("")
-        PathPlannerPath path11NBlueAuto = new PathPlannerPath​(List<Translation2d> bezierPoints,
-                                                            List<RotationTarget> holonomicRotations,
-                                                            List<ConstraintsZone> constraintZones,
-                                                            List<EventMarker> eventMarkers,
-                                                            PathConstraints globalConstraints,
-                                                            GoalEndState (0.0,0.0),
-                                                            boolean reversed,
-                                                            -120.24);
-    }*/
 }
--- src/main/java/frc/robot/commands/Auto/Auto31NBlueCommand.java
+++ src/main/java/frc/robot/commands/Auto/Auto31NBlueCommand.java
@@ -6,18 +6,18 @@ import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
-public class Auto11NRedCommand extends SequentialCommandGroup{
+public class Auto31NBlueCommand extends SequentialCommandGroup{
     private final SwerveSubsystem m_drivebase;
     private final ArmSubsystem m_arm;
     private final ShooterSubsystem m_shooter;
 
-    public Auto11NRedCommand(SwerveSubsystem drivebase, ArmSubsystem arm, ShooterSubsystem shooter) {
+    public Auto31NBlueCommand(SwerveSubsystem drivebase, ArmSubsystem arm, ShooterSubsystem shooter) {
         m_drivebase = drivebase;
         m_arm = arm;
         m_shooter = shooter;
-        addCommands(new AutoSpeakerCommand(m_shooter, m_arm).withTimeout(4.05),
+        addCommands(new AutoSpeakerCommand(m_shooter, m_arm),
                     new WaitCommand(0),
-                    m_drivebase.getAutonomousCommand("11NRed")
+                    m_drivebase.getAutonomousCommand("31NBlue")
         );
     }
 }
